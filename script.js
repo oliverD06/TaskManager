897462449ea352ce7605b8627e4df7e156a19eea
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2024-04-10 10:17:17 -0500

    Basics complete.

---
+++ script.js
@@ -0,0 +1,54 @@
+// TASK MANAGER
+// Using a Template Literal because it recognizes line breaks
+const menu = `TASK MANAGER;
+
+What would you like to do (Please enter one of the options below):
+"TASKS" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manager
+`;
+
+// Array for storing tasks
+const tasks = [
+    `Charge MacBook `,
+    `Master JavaScript`
+]
+
+// For displaying tasks to the user
+let showTasks = ``;
+
+// For storing the value of a new task
+let newTask;
+
+// For storing the number of the task to be removed
+let num;
+
+// For storing the value of the removed task
+let removed;
+
+// Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable
+let userInput = prompt(menu)
+
+// Loops and continues to display the menu until the user ends/closes the Task Manager (Until the user enters CLOSE)
+while(userInput !== "CLOSE"){
+    if (userInput = "TASKS") {
+        for(let i = 0; i<tasks.length;i++) {
+            showTasks+=(tasks[i]+"\n")
+        }
+        
+    }
+    alert(showTasks);
+
+    showTasks = '';
+// CODE TO COMPLETE TASK MANAGER SHOULD BE WRITTEN HERE
+
+    // Displays the menu again
+    userInput = prompt(menu)
+
+    // This break can be commented out after the close feature is complete
+    // break;
+}
+
+// Alerts the user that they have closed the program
+alert(`Thank you for using Task Manager`)
